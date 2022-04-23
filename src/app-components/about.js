949ab9d3e5cf0d354bfea8ea3a6f01448cf0cc55
caseymm <casey@bocoup.com>
2022-04-22 19:36:46 -0700

    links

--- src/app-components/about.js
+++ src/app-components/about.js
@@ -4,7 +4,7 @@ class Details extends React.Component {
   render() {
     return (
       <div className="about">
-        <div className="photo" style={{ backgroundImage: 'url("/portfolio/assets/yvonne.png")' }}></div>
+        <div className="photo" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/assets/yvonne.png)` }}></div>\
         <div className="text">
           <p>Yvonne M. Eadon (she/her), MLIS, is a doctoral candidate in Information Studies at the University of California, Los Angeles.</p> 
           <p>Her research exists at the intersection of conspiracy theory scholarship, information seeking and behavior, online misinformation/ 
--- src/app-components/cv.js
+++ src/app-components/cv.js
@@ -5,8 +5,8 @@ class Work extends React.Component {
     return (
       <div className="works">
         <h1>CV</h1>
-        <div className="standalone-link"><a href="/portfolio/assets/pdfs/Yvonne_Eadon_CV__2021.pdf">Link to CV</a></div>
-        <iframe className="cv" src="/portfolio/assets/pdfs/Yvonne_Eadon_CV__2021.pdf" width="100%" height="650px"></iframe>
+        <div className="standalone-link"><a href={`${process.env.PUBLIC_URL}/assets/pdfs/Yvonne_Eadon_CV__2021.pdf`}>Link to CV</a></div>
+        <iframe className="cv" src={`${process.env.PUBLIC_URL}/assets/pdfs/Yvonne_Eadon_CV__2021.pdf`} width="100%" height="650px"></iframe>
       </div>
     );
   }
